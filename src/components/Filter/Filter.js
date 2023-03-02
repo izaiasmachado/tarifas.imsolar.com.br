0f33e5a0a6fbc62775cd0d99a60733f46ff2186a
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-03-02 19:39:41 -0300

    feat: remove toggle filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,50 +1,26 @@
-import { useState, useRef } from "react";
+import React from "react";
 import "./styles.css";
 
 import { FormControl, MenuItem, Button } from "@material-ui/core";
 import TextField from "@mui/material/TextField";
-import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
-import CloseIcon from "@mui/icons-material/Close";
 
 export default function Toolbar({
   filters,
   handleClearFilters,
   handleChangeState,
 }) {
-  const dropdownRef = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <div className="Filter__container">
       <div className="filter">
-        <button
-          className="Filter__button"
-          onClick={() => setIsOpen(!isOpen)}
-          variant="contained"
-        >
-          <div className="Filter__button_content">
-            <div className="Filter__button_icon">
-              <FilterAltOutlinedIcon />
-            </div>
-            <div className="Filter__button_text">Filtro</div>
+        <div className="filter__dropdown">
+          <div className="Filter__dropdown_header">
+            <div className="filter__dropdown_title">Filtros</div>
           </div>
-        </button>
 
-        {isOpen && (
-          <div ref={dropdownRef} className="filter__dropdown">
-            <div className="Filter__dropdown_header">
-              <div className="filter__dropdown_title">Filtros</div>
-              <div
-                className="filter__dropdown_closeIcon"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                <CloseIcon />
-              </div>
-            </div>
-
-            <div className="detailed-search-container section">
-              <div className="detailed-search">
-                <div className="detailed-search-form">
+          <div className="detailed-search-container section">
+            <div className="detailed-search">
+              <div className="detailed-search-form">
+                <div className="Filter__filters">
                   {(filters || []).map((filter) => (
                     <FormControl
                       key={filter.key}
@@ -69,6 +45,8 @@ export default function Toolbar({
                       </TextField>
                     </FormControl>
                   ))}
+                </div>
+                <div className="Filter__button-container">
                   <Button
                     className="Filter__button-input"
                     variant="contained"
@@ -82,7 +60,7 @@ export default function Toolbar({
               </div>
             </div>
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
